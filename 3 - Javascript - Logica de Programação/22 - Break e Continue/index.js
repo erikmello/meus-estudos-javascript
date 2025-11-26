77be4c76cf7fcb865daece686ffe6ace42b1b9d8
Erik Mello <erickmello1993@hotmail.com>
2025-11-26 19:58:12 -0300

    Break e Continue 2

--- 3 - Javascript - Logica de Programação/22 - Break e Continue/index.js
+++ 3 - Javascript - Logica de Programação/22 - Break e Continue/index.js
@@ -41,20 +41,89 @@
 
 
 // exemplo Break
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let numero of numeros) {
+//     if (numero === 2 || numero === 5) {
+//         console.log('Pulei o número ' + numero);
+//         continue;
+//     }  // Explicação do continue: Pula a iteração atual quando o número for 2 ou 5 e vai para a próxima
+
+
+
+//     console.log(numero);
+//     if (numero === 7) {
+//         console.log('7 encontrado, saindo...');
+//         break; // Sai do loop quando o número for 7
+//     }  // Explicação do break: Interrompe completamente o loop quando o número for 7
+
+// }
+
+
+
+
+//Exemplo com while e do while
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let i = 0;
+// while (i < numeros.length) {
+//     let numero = numeros[i];
+
+//     if (numero === 2) {
+//         console.log("Pulei o número 2 ");
+//         i++;
+//         continue; // Pula a iteração atual quando o número for 2
+//     }
+
+
+//     console.log(numero);
+
+
+//     if (numero === 7) {
+//         console.log("7 encontrado, saindo...");
+//         i++;
+//         break; // Sai do loop quando o número for 7
+//     }
+//     i++;
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+//do while
+
+//Continue continua para proxima iteração do loop
+//Break sai do loop completamente
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for (let numero of numeros) {
-    if (numero === 2 || numero === 5) {
-        console.log('Pulei o número ' + numero);
-        continue;
-    }  // Explicação do continue: Pula a iteração atual quando o número for 2 ou 5 e vai para a próxima
+let i = 0;
+do {
+    let numero = numeros[i];
 
+    if (numero === 2) {
+        console.log("Pulei o número 2 ");
+        i++;
+        continue; // Pula a iteração atual quando o número for 2
+    }
 
 
     console.log(numero);
-    if (numero === 7) {
-        console.log('7 encontrado, saindo...');
-        break; // Sai do loop quando o número for 7
-    }  // Explicação do break: Interrompe completamente o loop quando o número for 7
 
-}
+
+    if (numero === 7) {
+        console.log("7 encontrado, saindo...");
+        i++;
+        break; // Sai do loop quando o número for 7
+    }
+    i++;
+
+} while (i < numeros.length);
